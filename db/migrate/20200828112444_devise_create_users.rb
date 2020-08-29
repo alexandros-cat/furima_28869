273e333841c4d7a2af20a2607a2f0fa573e95b32
alexandros-cat <alexandros.cat@gmail.com>
2020-08-29 14:37:12 +0900

    usersテーブルの作成

--- db/migrate/20200828112444_devise_create_users.rb
+++ db/migrate/20200828112444_devise_create_users.rb
@@ -4,15 +4,21 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :family_name,      null: false, default: ""
+      t.string :first_name,       null: false, default: ""
+      t.string :family_name_kana, null: false, default: ""
+      t.string :first_name_kana,  null: false, default: ""
+      t.string :nickname ,        null: false, default: ""
+      t.string :email ,           null: false, default: ""
+      t.string :password,         null: false, default: ""
+      t.date   :birthday ,        null: false 
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      #t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
