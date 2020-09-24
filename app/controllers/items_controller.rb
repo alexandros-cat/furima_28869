583cec90238aeeef98c5dc31786fe6d935dc37a1
alexandros-cat <alexandros.cat@gmail.com>
2020-09-24 21:16:50 +0900

    jaymlファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,8 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if@item.valid? 
+      @item.save
        redirect_to root_path
     else
        render 'new'
